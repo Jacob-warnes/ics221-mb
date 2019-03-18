405267148c232efac6c3652227f076dba161dd16
jacob <jacob.warnes21@online.camosun.ca>
2019-03-18 13:16:58 -0700

    lab7 task 3

--- api_server/routes/api_router.js
+++ api_server/routes/api_router.js
@@ -2,9 +2,14 @@ const express = require('express');
 const api_router = express.Router();
 
 const msgAPIController = require('../controllers/msg-api');
+const userAPIController = require('../controllers/user-api');
 
 api_router.route('/msgs')
 .get(msgAPIController.getAllMessagesOrderedByLastPosted)
 .post(msgAPIController.addNewMessage);
+
+//Is this right?
+api_router.post('/users', userAPIController.registerNewUser);
+api_router.get('/users/login', userAPIController.loginUser);
 
 module.exports = api_router;
